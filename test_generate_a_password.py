85327162eafb9efbfbbd883e8b47c5b76cfb7bd6
Emmanuel Cortes <polosetroc@gmail.com>
2021-07-25 16:55:55 -0700

    small change to test module for generate_a_password and added diceware file

--- test_generate_a_password.py
+++ test_generate_a_password.py
@@ -3,7 +3,7 @@ from unittest import TestCase
 
 class Test_generate_diceware_password(TestCase):
     def test_generate_diceware_password(self):
-        from generate_a_password import generate_diceware_password, solution
+        from generate_a_password import generate_diceware_password
         pass_phase_words = 5
         # check if password contains the specified amount of words
-        self.assertEqual(len(solution(pass_phase_words).split()), pass_phase_words)
+        self.assertEqual(len(generate_diceware_password(pass_phase_words).split()), pass_phase_words)
